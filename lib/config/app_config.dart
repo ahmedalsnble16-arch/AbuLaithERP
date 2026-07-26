bb6fe01267c0c5525f2e1942a96f8dc18faa593a
ahmedalsnble16-arch <ahmedalsnble16@gmail.com>
2026-07-26 20:57:40 +0300

    Update app_config.dart

---
+++ lib/config/app_config.dart
@@ -0,0 +1,17 @@
+class AppConfig {
+  static const String appName = 'أبو ليث ERP';
+  static const String appVersion = '1.0.0';
+  static const String companyName = 'معمل أبو ليث';
+  static const String companyActivity = 'إنتاج الكيك والنواشف';
+  static const String currency = 'ريال يمني';
+  static const String currencySymbol = 'ر.ي';
+  static const String dbName = 'abu_laith_erp.db';
+  static const int dbVersion = 1;
+  static const int sessionTimeout = 30;
+  static const int maxLoginAttempts = 5;
+  static const String syncInterval = '5';
+  static const int maxRetries = 3;
+  static const int defaultBoxSize = 60;
+  static const int lowStockThreshold = 100;
+  static const int pageSize = 20;
+}
