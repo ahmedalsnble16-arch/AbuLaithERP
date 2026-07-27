1bc96fbffefde2b8c4ca8eebf52d8d26074b3eb3
ahmedalsnble16-arch <ahmedalsnble16@gmail.com>
2026-07-27 14:59:26 +0300

    Update audit_log_repository.dart[skip ci]

---
+++ lib/data/repositories/audit_log_repository.dart
@@ -0,0 +1,11 @@
+import '../../core/constants/db_constants.dart';
+import '../../core/database/database_helper.dart';
+
+class AuditLogRepository {
+  final DatabaseHelper _db = DatabaseHelper();
+
+  Future<List<Map<String, dynamic>>> getAll() async {
+    final database = await _db.database;
+    return database.query(DBConstants.tableAuditLogs, orderBy: 'created_at DESC');
+  }
+}
