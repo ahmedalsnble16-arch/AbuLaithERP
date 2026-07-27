11c332ba26998642eecfacf2c4deda042a83da86
ahmedalsnble16-arch <ahmedalsnble16@gmail.com>
2026-07-27 16:46:04 +0300

    Update printer.dart[skip ci]

--- lib/core/utils/printer.dart
+++ lib/core/utils/printer.dart
@@ -1,7 +1,11 @@
+import 'dart:io';
 import 'package:printing/printing.dart';
 
 class PrinterService {
   static Future<void> printPdf(String filePath) async {
-    await Printing.layoutPdf(onLayout: (_) => File(filePath).readAsBytes());
+    final file = File(filePath);
+    if (await file.exists()) {
+      await Printing.layoutPdf(onLayout: (_) => file.readAsBytes());
+    }
   }
 }
