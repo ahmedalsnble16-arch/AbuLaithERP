e0a7405322e27076f01293f196fa2dab1061ce57
ahmedalsnble16-arch <ahmedalsnble16@gmail.com>
2026-07-27 15:28:21 +0300

    Update product_service.dart[skip ci]

---
+++ lib/data/services/product_service.dart
@@ -0,0 +1,22 @@
+import '../models/product.dart';
+import '../repositories/product_repository.dart';
+
+class ProductService {
+  final ProductRepository _repository = ProductRepository();
+
+  Future<List<Product>> getAllProducts({String? search}) async {
+    return await _repository.getAll(search: search);
+  }
+
+  Future<String> addProduct(Product product) async {
+    return await _repository.add(product);
+  }
+
+  Future<void> updateProduct(Product product) async {
+    await _repository.update(product);
+  }
+
+  Future<void> deleteProduct(String id) async {
+    await _repository.delete(id);
+  }
+}
