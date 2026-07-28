b89e6944852219aabc7090f2f799f9cec4316383
ahmedalsnble16-arch <ahmedalsnble16@gmail.com>
2026-07-29 00:33:47 +0300

    Update main.dart[skip ci]

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'app.dart';
 import 'core/database/database_helper.dart';
+import 'core/auth/session_manager.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
