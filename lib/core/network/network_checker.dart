0bca42bdbaa4bb63a6f0ef52cae4256af71851cc
ahmedalsnble16-arch <ahmedalsnble16@gmail.com>
2026-07-29 00:36:44 +0300

    Update network_checker.dart[skip ci]

--- lib/core/network/network_checker.dart
+++ lib/core/network/network_checker.dart
@@ -3,6 +3,6 @@ import 'package:connectivity_plus/connectivity_plus.dart';
 class NetworkChecker {
   static Future<bool> hasInternet() async {
     final result = await Connectivity().checkConnectivity();
-    return result != ConnectivityResult.none;
+    return !result.contains(ConnectivityResult.none);
   }
 }
