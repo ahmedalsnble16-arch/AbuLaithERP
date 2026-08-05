2cac4bad5abe07d27ce8d2cd0d7e50d72d0df995
ahmedalsnble16-arch <ahmedalsnble16@gmail.com>
2026-08-05 23:33:28 +0300

    Update app_config.dart[skip ci]

--- lib/config/app_config.dart
+++ lib/config/app_config.dart
@@ -6,7 +6,7 @@ class AppConfig {
   static const String currency = 'ريال يمني';
   static const String currencySymbol = 'ر.ي';
   static const String dbName = 'abu_laith_erp.db';
-  static const int dbVersion = 1;
+  static const int dbVersion = 2; // تم تغييره من 1 إلى 2
   static const int sessionTimeout = 30;
   static const int maxLoginAttempts = 5;
   static const String syncInterval = '5';
